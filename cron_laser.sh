ed51f250e22eeb44d231e7da2e3d9e577f3dabce
dutchpatriot <dutchpatriot@gmail.com>
2017-02-05 04:45:09 +0100

    Add files via upload

---
+++ cron_laser.sh
@@ -0,0 +1,23 @@
+#! /bin/sh 
+
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 0.7 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 0.9 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 1 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 1.2 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 1.4 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 1.7 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 1.9 
+sleep 2
+sudo laserengraver -f /home/pi/laserengraver/Gcode/dragon1_0001.ngc  -s 2.5 
+sleep 2
+
+
+
+exit
+
